a23815102f50e889859113a1db8de02c6126fe25
Abhay vishwakarma <142080096+Abhay7111@users.noreply.github.com>
2023-08-15 14:30:38 +0530

    html css or js update

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+var crsr = document.querySelector(".cursor")
+var crsrblr = document.querySelector(".cursorblr")
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+})
+
+document.addEventListener("mousemove",function(dets){
+    crsrblr.style.left = dets.x -100 +"px"
+    crsrblr.style.top = dets.y -100 +"px"
+})
+
 gsap.to("header",{
     backgroundColor :"#fff",
     height:"70px",
@@ -5,10 +17,11 @@ gsap.to("header",{
     scrollTrigger:{
         trigger:"header",
         scroller:"body",
-        // markers:"true",
-        start:"top -10%",
-        end:"top -11%",
-        scrub:1.3,
+        markers:"true",
+        start:"top -5%",
+        end:"top -10%",
+        scrub:2,
+        top:"120px",
     },
 })
 
@@ -16,10 +29,25 @@ gsap.to("#main",{
     backgroundColor:"#000",
     scrollTrigger:{
         trigger:"#main",
-        scroll:"#main",
-        markers:true,
+        scroll:".page1",
+        markers:"true",
         start:"top 10%",
-        end:"top -70%",
+        end:"top -20%",
         scrub:2,
     }
+})
+
+gsap.to(".page2",{
+    backgroundColor :"#000",
+    height:"100vh",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:".page2",
+        scroller:"body",
+        markers:"true",
+        start:"top -5%",
+        end:"top -4vh",
+        scrub:4,
+        top:"120px",
+    },
 })
